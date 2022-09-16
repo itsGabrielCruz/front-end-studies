19ced39e924a3a645d2baad9071f5b6689d66032
Gabriel Cruz <itsgabrielcruz@gmail.com>
2022-09-15 23:27:40 -0300

    display-grid

---
+++ display-grid/script.js
@@ -0,0 +1,11 @@
+var brasilCamp = [1959, 1962, 1970, 1994, 2002];
+
+// for(let i = 0; i < brasilCamp.length; i++) {
+//     console.log(`O Brasil ganhou a copa de ${brasilCamp[i]}`);
+    
+//     if(brasilCamp[i] === 1994) {
+//         break;
+//     }
+// }
+
+var ultimoNum = brasilCamp[brasilCamp.length -1];
